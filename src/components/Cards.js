4dde8b997486285c7be3c98f23eb2cb9d6a80428
Adeleadbe <adbe21@gmail.com>
2023-01-03 12:54:09 +0100

    Cards css terminé

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,17 +1,19 @@
 import React from "react";
 import logements from "../data/logements.json";
+import { Link } from "react-router-dom";
 
-export default function Cards({}) {
+export default function Cards() {
   const data = JSON.stringify(logements);
   const parseData = JSON.parse(data);
   return (
     <div className="card_container">
       {parseData.map((logement) => {
         return (
-          <div className="card" key={logement.id}>
+          <Link to="/jesaispas" className="card" key={logement.id}>
             <img className="card_image" src={logement.cover} alt="Logement" />
+            <div className="card_mask"></div>
             <h2> {logement.title} </h2>
-          </div>
+          </Link>
         );
       })}
     </div>
