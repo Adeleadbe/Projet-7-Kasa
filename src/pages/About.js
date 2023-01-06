da25fd87e263d1b965c3fcb7edfff2b8248dda7b
Adeleadbe <adbe21@gmail.com>
2023-01-06 10:04:37 +0100

    Création du composant collapse

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Collapse from "../components/Collapse"
+import NavBar from "../components/NavBar"
 
 export default function About() {
   return (
-    <div>About</div>
+    <main className="about_navbar">
+      <NavBar /> 
+      <div className='collapse'>
+
+      </div>
+    </main>
   )
 }
